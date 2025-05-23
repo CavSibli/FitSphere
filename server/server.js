50362675a241b5c12baa2aa351ecc8e835600ff7
Sibli Cav <cav.sibligs@gmail.com>
2025-05-23 19:07:34 +0200

    Feat: tentative deploiement2

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
     ? process.env.FRONTEND_URL 
-    : 'http://localhost:5173',
+    : 'https://fit-sphere-kurp-ghoqrj7i5-siblis-projects.vercel.app',
   credentials: true
 }));
 app.use(express.json());
